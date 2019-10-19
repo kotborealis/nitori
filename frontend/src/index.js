0467b77ac8a71148079d5449be76cc7d69ca5ad8
kotborealis <kotborealis@gmail.com>
2019-10-19 21:45:25 +0300

    pavel-like

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import {render} from 'react-dom';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+render(
+    <div>Hello, world!</div>,
+    document.getElementById('App')
+);
