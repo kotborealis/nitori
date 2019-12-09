49e6b28f7b4aa40a76aca2da1a51d45f8f9e3f93
Евгений <kotborealis@awooo.ru>
2019-12-09 14:25:05 +0300

    refactored store

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -5,17 +5,25 @@ import {apiStoreHelper, fetchStoreHelper} from './helpers';
 const store = (set) => ({
     ...apiStoreHelper("widgets", set, `/widgets/`, []),
 
-    ...apiStoreHelper("testSpecs", set, (widgetId) => `/widgets/${widgetId}/test-specs/`, []),
+    ...apiStoreHelper("testSpecs", set,
+        (widgetId) => `/widgets/${widgetId}/test-specs/`,
+        []),
 
-    ...apiStoreHelper("testTargets", set, (widgetId) => `/widgets/${widgetId}/test-targets/`, []),
+    ...apiStoreHelper("testTargets", set,
+        (widgetId) => `/widgets/${widgetId}/test-targets/`,
+        []),
 
-    ...fetchStoreHelper("userData", set, `/auth/user_data.php`, {}),
+    ...fetchStoreHelper("userData", set,
+        `/auth/user_data.php`,
+        {}),
 
     ...apiStoreHelper("testTarget", set,
-        (widgetId, testTargetId) => `/widgets/${widgetId}/test-targets/${testTargetId}`, {}),
+        (widgetId, testTargetId) => `/widgets/${widgetId}/test-targets/${testTargetId}`,
+        {}),
 
     ...apiStoreHelper("testTargetSubmit", set,
-        (widgetId, testSpecId) => `/widgets/${widgetId}/test-targets/?testSpecId=${testSpecId}`, null,
+        (widgetId, testSpecId) => `/widgets/${widgetId}/test-targets/?testSpecId=${testSpecId}`,
+        null,
         {
             method: "POST"
         }
