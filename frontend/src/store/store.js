29a1459795cb4e3d504d171e6783c62ea11c014f
Евгений <kotborealis@awooo.ru>
2019-12-10 10:03:14 +0300

    docs

--- frontend/src/store/store.js
+++ frontend/src/store/store.js
@@ -4,6 +4,18 @@ import {generateFetchStore} from './helpers';
 import {api, fetchJSON} from '../api';
 import {produce} from 'immer';
 
+/**
+ * Store props, controlled by fetch functions (see ./api/: fetchJSON, fetchPlainText, api)
+ *
+ * For each [name] prop in creates next structure in the store:
+ * name: {fetch, data, loading, error, ready}
+ *
+ * * fetch --- calls specified fetch function and sets data, loading, error and ready accordingly to it's execution
+ * * data --- return value of fetch function, null by default
+ * * loading --- loading status, true/false, null by default
+ * * error --- error thrown by fetch function, null by default
+ * * ready --- true if not loading and no error, false otherwise
+ */
 const storeFetchControlled = (set, get) => ({
     widgets: () => api(`/widgets/`),
 
@@ -22,12 +34,20 @@ const storeFetchControlled = (set, get) => ({
     userData: () => fetchJSON(`/auth/user_data.php`)
 });
 
+/**
+ * Usual store
+ *
+ * Set --- immer_ized set function
+ */
 const store = (set, get) => ({
     set: fn => set(produce(fn), "set"),
 
     widgetId: null,
 });
 
+/**
+ * Compose stores
+ */
 export const [useStore, storeApi] =
     create(
         devtools((set, get) => ({
