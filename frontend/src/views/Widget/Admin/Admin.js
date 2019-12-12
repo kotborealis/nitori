fa5ac2bfc35a10c36026111d3cf95327a73b5441
Евгений <kotborealis@awooo.ru>
2019-12-12 17:09:30 +0300

    refactored admin page

--- frontend/src/views/Widget/Admin/Admin.js
+++ frontend/src/views/Widget/Admin/Admin.js
@@ -1,16 +1,14 @@
 import React, {useEffect} from 'react';
 import {TestSpecsList} from '../../../components/TestSpec/TestSpecsList';
-import {TestSpecCreate} from '../../../components/TestSpec/TestSpecCreate';
 import {TestTargetsList} from '../../../components/TestTarget/TestTargetsList';
 import {useStore} from '../../../store/store';
-import {useParams} from 'react-router-dom';
 import Grid from '@material-ui/core/Grid';
 import {Typography} from '@material-ui/core';
 import {Loading} from '../../../components/InvalidState/Loading';
 import {Error} from '../../../components/InvalidState/Error';
 
 export default () => {
-    const {widgetId} = useParams();
+    const widgetId = useStore(state => state.widgetId);
 
     // Fetch test targets
     const fetchTestTargets = useStore(state => state.testTargets.fetch);
@@ -31,10 +29,6 @@ export default () => {
     return (
         <Grid container direction="column" spacing={10}>
             <Grid item>
-                <Typography variant="h5">Добавить тест</Typography>
-                <TestSpecCreate/>
-            </Grid>
-            <Grid item>
                 <Typography variant="h5">Список тестов</Typography>
                 {testSpecsLoading && <Loading/>}
                 {testSpecsError && <Error error={testSpecsError}/>}
@@ -44,12 +38,11 @@ export default () => {
             </Grid>
             <Grid item>
                 <Typography variant="h5">Список таргетов</Typography>
-                {testTargetsLoading || testSpecsLoading && <Loading/>}
-                {testTargetsError || testSpecsError && <Error error={testTargetsError}/>}
-                {!testSpecsLoading && !testSpecsError && !testTargetsLoading && !testTargetsError &&
+                {testTargetsLoading && <Loading/>}
+                {testTargetsError && <Error error={testTargetsError}/>}
+                {!testTargetsLoading && !testTargetsError &&
                  <TestTargetsList
                      data={testTargetsData}
-                     testSpecs={testSpecsData}
                  />}
             </Grid>
         </Grid>
