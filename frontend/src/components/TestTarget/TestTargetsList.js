462512f19a3c8da10dc398949dda378a4e832250
Евгений <kotborealis@awooo.ru>
2020-01-22 12:50:35 +0300

    Reworked TestTargetsList using material-table

--- frontend/src/components/TestTarget/TestTargetsList.js
+++ frontend/src/components/TestTarget/TestTargetsList.js
@@ -1,73 +1,57 @@
 import React from 'react';
 
-import Table from '@material-ui/core/Table';
-import TableBody from '@material-ui/core/TableBody';
-import TableCell from '@material-ui/core/TableCell';
-import TableHead from '@material-ui/core/TableHead';
-import TableRow from '@material-ui/core/TableRow';
-import Paper from '@material-ui/core/Paper';
 import {exitCodeToIcon} from '../../helpers/exitCodeToIcon';
 import Chip from '@material-ui/core/Chip';
 import {exitCodeToColor} from '../../helpers/exitCodeToColor';
-import {Link} from 'react-router-dom';
 import {useStore} from '../../store/store';
 import {TimeUpdated} from '../TimeUpdated/TimeUpdated';
+import MaterialTable from 'material-table';
+import {useHistory} from 'react-router-dom';
 
 export const TestTargetsList = ({data}) => {
+    const history = useHistory();
     const widgetId = useStore(state => state.widgetId);
 
     return (
-        <Paper>
-            <Table>
-                <TableHead>
-                    <TableRow>
-                        <TableCell>Тест</TableCell>
-                        <TableCell>Пользователь</TableCell>
-                        <TableCell>Отправлено</TableCell>
-                        <TableCell>Проверки</TableCell>
-                    </TableRow>
-                </TableHead>
-                <TableBody>
-                    {data.map(({
-                                   _id,
-                                   timestamp,
-                                   userData: {name, login},
-                                   testSpecId,
-                                   compilerResult,
-                                   linkerResult,
-                                   runnerResult,
-                                   testSpec
-                               }) =>
-                        <TableRow
-                            component={Link}
-                            to={`/widgets/${widgetId}/test-targets/${_id}`}
-                            style={{textDecoration: 'none'}}
-                        >
-                            <TableCell>{testSpec.name}</TableCell>
-                            <TableCell>{name ? `${name} (${login})` : login}</TableCell>
-                            <TableCell><TimeUpdated>{timestamp}</TimeUpdated></TableCell>
-                            <TableCell>
-                                {(() => {
-                                    const CompilerIcon = exitCodeToIcon(compilerResult.exitCode);
-                                    const LinkerIcon = exitCodeToIcon(linkerResult.exitCode);
-                                    const RunnerIcon = exitCodeToIcon(runnerResult.exitCode);
+        <MaterialTable
+            onRowClick={(event, rowData) => {
+                history.push(`/widgets/${widgetId}/test-targets/${rowData._id}`);
+            }}
+            columns={[
+                {
+                    title: 'Тест',
+                    render: ({testSpec: {name}}) => name
+                },
+                {
+                    title: 'Пользователь',
+                    render: ({userData: {name, login}}) => name ? `${name} (${login})` : login
+                },
+                {
+                    title: 'Отправлено',
+                    field: 'timestamp',
+                    render: ({timestamp}) => <TimeUpdated>{timestamp}</TimeUpdated>
+                },
+                {
+                    title: 'Проверки',
+                    render: ({compilerResult, linkerResult, runnerResult}) => {
+                        const CompilerIcon = exitCodeToIcon(compilerResult.exitCode);
+                        const LinkerIcon = exitCodeToIcon(linkerResult.exitCode);
+                        const RunnerIcon = exitCodeToIcon(runnerResult.exitCode);
 
-                                    const compilerColor = exitCodeToColor(compilerResult.exitCode);
-                                    const linkerColor = exitCodeToColor(linkerResult.exitCode);
-                                    const runnerColor = exitCodeToColor(runnerResult.exitCode);
+                        const compilerColor = exitCodeToColor(compilerResult.exitCode);
+                        const linkerColor = exitCodeToColor(linkerResult.exitCode);
+                        const runnerColor = exitCodeToColor(runnerResult.exitCode);
 
-                                    return (
-                                        <>
-                                            <Chip icon={<CompilerIcon/>} color={compilerColor} label="Компиляция"/>
-                                            <Chip icon={<LinkerIcon/>} color={linkerColor} label="Линковка"/>
-                                            <Chip icon={<RunnerIcon/>} color={runnerColor} label="Тесты"/>
-                                        </>
-                                    );
-                                })()}
-                            </TableCell>
-                        </TableRow>)}
-                </TableBody>
-            </Table>
-        </Paper>
+                        return (
+                            <>
+                                <Chip icon={<CompilerIcon/>} color={compilerColor} label="Компиляция"/>
+                                <Chip icon={<LinkerIcon/>} color={linkerColor} label="Линковка"/>
+                                <Chip icon={<RunnerIcon/>} color={runnerColor} label="Тесты"/>
+                            </>
+                        );
+                    }
+                }
+            ]}
+            data={data}/>
     );
 };
