24b0e5e96bb89c2659df11673f147f911abebca9
Евгений <kotborealis@awooo.ru>
2020-03-13 11:11:32 +0300

    updated configuration & mock-api

--- backend/.config.dev.js
+++ backend/.config.dev.js
@@ -1,8 +1,14 @@
 module.exports = {
     ...require('./.config.js'),
+    api: {
+        port: 3001,
+        limits: {
+            fileSize: 1024 * 10
+        }
+    },
     docker: undefined,
     database: {
-        url: 'http://admin:123qwe@localhost:5984',
+        url: 'http://admin:123qwe@192.168.99.100:5984',
         name: 'nitori'
     }
 };
--- frontend/mock-api/mock-api.js
+++ frontend/mock-api/mock-api.js
@@ -1,8 +1,8 @@
 module.exports = [
     {
         context: ['/api/v1'],
-        target: 'http://127.0.0.1:3000',
-        pathRewrite: {'^/api/v1' : ''},
+        target: `http://127.0.0.1:${require('../../backend/.config.dev.js').api.port}`,
+        pathRewrite: {'^/api/v1': ''},
         headers: {
             Cookie: 'PHPSESSID=qwerty'
         }
