a67fb107c06c4fa4fa613fa151579224bc985faa
Евгений <kotborealis@awooo.ru>
2020-07-03 10:36:52 +0300

    Logging w/ winston

---
+++ backend/src/logging/logger.js
@@ -0,0 +1,26 @@
+const {getCorrelationId} = require('../correlation/correlation');
+const {createLogger, format, transports} = require('winston');
+const {combine, timestamp, label, json, errors, simple, colorize} = format;
+
+const logger = (serviceName) =>
+    createLogger({
+        level: 'debug',
+        defaultMeta: {serviceName},
+        format: combine(
+            format((info) => {
+                info.label = `${serviceName}/${getCorrelationId()}`;
+                return info;
+            })(),
+            timestamp({
+                format: `YYYY-MM-DD HH:mm:ss'`
+            }),
+            errors({stack: true}),
+            colorize(),
+            simple(),
+        ),
+        transports: [
+            new transports.Console()
+        ],
+    });
+
+module.exports = logger;
