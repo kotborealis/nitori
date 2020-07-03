ed6d272bfdda5b77f993206f13d48fee3fc0f9d6
Евгений <kotborealis@awooo.ru>
2020-07-03 11:03:36 +0300

    Tests for correlation.js

---
+++ backend/src/correlation/correlation.test.js
@@ -0,0 +1,36 @@
+const {
+    getCorrelationId,
+    runWithCorrelation,
+    NO_CORRELATION_ID
+} = require('./correlation');
+
+describe('correlation', () => {
+    it('returns NO_CORRELATION_ID out of context', () => {
+        expect(getCorrelationId()).toEqual(NO_CORRELATION_ID);
+    });
+
+    it('returns id when ran with correlation', (done) => {
+        runWithCorrelation(() => {
+            expect(getCorrelationId()).toBeTruthy();
+            done();
+        });
+    });
+
+    it('returns set id when ran with correlation with id', (done) => {
+        const ID = `correlation-test`;
+        runWithCorrelation(() => {
+            expect(getCorrelationId()).toEqual(ID);
+            done();
+        }, ID);
+    });
+
+    it('works in nested calls', (done) => {
+        runWithCorrelation(() =>
+            setImmediate(() =>
+                setInterval(() => {
+                    expect(getCorrelationId()).toBeTruthy();
+                    done();
+                }, 100))
+        );
+    });
+});
