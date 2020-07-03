d102d576ba32bfd906c5ef32674e2c0e3bff6726
Евгений <kotborealis@awooo.ru>
2020-07-03 10:37:11 +0300

    refactoring

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,7 +4,7 @@ const config = require('chen.js').config('.config.js');
 const {init: databaseInit} = require('./database/');
 const {API} = require('./api');
 
-process.on('unhandledRejection', (reason) => debug('unhandledRejection', reason));
+process.on('unhandledRejection', (reason) => console.error('unhandledRejection', reason));
 
 (async () => {
     databaseInit(config);
