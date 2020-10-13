9e4f356735cbe0607e7ecd39d64abcfa0c002629
kotborealis <kotborealis@awooo.ru>
2020-10-13 09:46:15 +0300

    Added Sandbox.build to backend init

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,10 +3,13 @@ require('./logging/logger').init(config);
 
 const {init: databaseInit} = require('./database/');
 const {API} = require('./api');
+const {Docker} = require('node-docker-api');
+const {Sandbox} = require('./Sandbox');
 
 process.on('unhandledRejection', (reason) => console.error('unhandledRejection', reason));
 
 (async () => {
+    await Sandbox.build(new Docker(config.docker), config);
     databaseInit(config);
     API(config);
 })();
