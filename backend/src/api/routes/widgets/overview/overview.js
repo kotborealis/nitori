a8a1defe4f8047a1e6c4ee96dee2b17724e1c8bc
kotborealis <kotborealis@awooo.ru>
2020-10-21 14:59:02 +0300

    api: overview testtargets by user/test-specs or test-specs/user

---
+++ backend/src/api/routes/widgets/overview/overview.js
@@ -0,0 +1,187 @@
+const {Router} = require('express');
+const {TestSpecModel, TestTargetModel} = require('../../../../database');
+const {Types: {ObjectId}} = require('mongoose');
+
+module.exports = (config) => {
+
+    const router = Router();
+
+    router.route('/user/test-specs')
+        .get(async (req, res) => {
+
+            const widgetId = ObjectId(req.widgetId);
+
+            /**
+             * На выходе получаем массив структур:
+             * {
+             *     userData: <object>
+             *     testTargetsByTestSpec: <Array{
+             *         testSpec: <TestSpec>
+             *         testTargets: <Array{<TestTarget>}>
+             *     }>
+             * }
+             */
+            const testTargets = await TestTargetModel
+                .aggregate([
+                    {
+                        '$match': {
+                            'widget': widgetId
+                        }
+                    }, {
+                        '$group': {
+                            '_id': {
+                                'userId': '$userData.userId',
+                                'testSpec': '$testSpec'
+                            },
+                            'userData': {
+                                '$first': '$userData'
+                            }
+                        }
+                    }, {
+                        '$group': {
+                            '_id': '$_id.userId',
+                            'testTargetsByTestSpec': {
+                                '$push': '$_id.testSpec'
+                            },
+                            'userData': {
+                                '$first': '$userData'
+                            }
+                        }
+                    }, {
+                        '$project': {
+                            '_id': false
+                        }
+                    }, {
+                        '$lookup': {
+                            'let': {
+                                'widget': widgetId,
+                                'userId': '$userData.userId'
+                            },
+                            'from': 'testtargets',
+                            'as': 'testTargetsByTestSpec',
+                            'pipeline': [
+                                {
+                                    '$match': {
+                                        '$expr': {
+                                            '$and': [
+                                                {
+                                                    '$eq': [
+                                                        '$widget', '$$widget'
+                                                    ]
+                                                }, {
+                                                    '$eq': [
+                                                        '$userData.userId', '$$userId'
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    }
+                                }, {
+                                    '$sort': {
+                                        'timestamp': -1
+                                    }
+                                }, {
+                                    '$group': {
+                                        '_id': '$testSpec',
+                                        'testTargets': {
+                                            '$push': '$$ROOT'
+                                        }
+                                    }
+                                }, {
+                                    '$lookup': {
+                                        'from': 'testspecs',
+                                        'localField': '_id',
+                                        'foreignField': '_id',
+                                        'as': 'testSpec'
+                                    }
+                                }, {
+                                    '$unwind': '$testSpec'
+                                }, {
+                                    '$project': {
+                                        '_id': false
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]);
+
+            res.mongo(testTargets);
+        });
+
+    router.route('/test-specs/user')
+        .get(async (req, res) => {
+
+            const widgetId = ObjectId(req.widgetId);
+
+            /**
+             * На выходе получаем массив структур:
+             * {
+             *     testSpec: <object>
+             *     testTargetsByUser: <Array{
+             *         userData: <object>
+             *         testTargets: <Array{<TestTarget>}>
+             *     }>
+             * }
+             */
+            const testTargets = await TestSpecModel
+                .aggregate([
+                    {
+                        '$match': {
+                            'widget': widgetId
+                        }
+                    }, {
+                        '$project': {
+                            'testSpec': '$$ROOT',
+                            '_id': false
+                        }
+                    }, {
+                        '$lookup': {
+                            'let': {
+                                'widget': widgetId,
+                                'testSpec': '$testSpec._id'
+                            },
+                            'from': 'testtargets',
+                            'as': 'testTargetsByUser',
+                            'pipeline': [
+                                {
+                                    '$match': {
+                                        '$expr': {
+                                            '$and': [
+                                                {
+                                                    '$eq': [
+                                                        '$testSpec', '$$testSpec'
+                                                    ]
+                                                }
+                                            ]
+                                        }
+                                    }
+                                }, {
+                                    '$sort': {
+                                        'timestamp': -1
+                                    }
+                                }, {
+                                    '$group': {
+                                        '_id': '$userData.userId',
+                                        'userData': {
+                                            '$first': '$userData'
+                                        },
+                                        'testTargets': {
+                                            '$push': '$$ROOT'
+                                        }
+                                    }
+                                }, {
+                                    '$project': {
+                                        '_id': false
+                                    }
+                                }
+                            ]
+                        }
+                    }
+                ]);
+
+            res.mongo(testTargets);
+        });
+
+    return router;
+};
